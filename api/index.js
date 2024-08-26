a5e7169a2753c3a50c50643bb2e3d0a6f3eacee4
Sonu Kumar <sonu82256@gmail.com>
2024-08-26 21:56:40 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const PORT = 3000
+
+app.get('/api', function (req, res) {
+  res.send("hello")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on port ${PORT}`)
+})
