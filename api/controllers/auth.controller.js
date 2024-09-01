06abb28b6637125bdd37254baefbaafe71accd29
Sonu Kumar <sonu82256@gmail.com>
2024-09-01 13:01:26 +0530

    sign-in created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,20 +1,43 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
 
 export const testUser = (req, res) => {
     res.send("test is working");
 };
 
-export const signup = async(req, res) => {
+export const signup = async (req, res) => {
     // console.log(req.body);
-    const {username, email, phone, password} = req.body;
+    const { username, email, phone, password } = req.body;
     const salt = 10;
     const hashedPassword = bcryptjs.hashSync(password, salt);
-    const newUser = new User({username, email, phone, password: hashedPassword});
+    const newUser = new User({
+        username,
+        email,
+        phone,
+        password: hashedPassword,
+    });
     try {
         await newUser.save();
-        res.send("user created successfully")
+        res.status(201).json("User created successfully");
     } catch (error) {
-        console.log("error signup"+error);
+        console.log("error signup" + error);
+        res.send("error signup" + error);
+    }
+};
+
+export const signin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const validUser = await User.findOne({ email });
+        if (!validUser) res.send("invalid email id");
+        const validPassword = bcryptjs.compareSync(
+            password,
+            validUser.password
+        );
+        if (!validPassword) res.send("invalid password");
+        console.log(validUser);
+        res.send("sign-in successfully");
+    } catch (error) {
+        console.log(error);
     }
 };
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {testUser, signup} from "../controllers/auth.controller.js";
+import {testUser, signup, signin} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.get("/test", testUser);
 router.post("/sign-up", signup);
+router.get("/sign-in", signin);
 
 //api/user/update
 
